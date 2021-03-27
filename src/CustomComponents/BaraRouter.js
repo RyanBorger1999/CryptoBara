a3dc6991cbf282c0abc5ac77a7844cd6a33fe455
Ryan Borger <ryanborger1999@gmail.com>
2021-03-26 20:54:58 -0400

    Custom Components Folder

---
+++ src/CustomComponents/BaraRouter.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+function BaraRouter() {
+  return (
+    <Router>
+      <div className="BaraRouter">
+      </div>
+    </Router>
+  );
+}
+
+export default BaraRouter;
