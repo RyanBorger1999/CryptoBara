c71db2286f836c1a0ffe32f26657d8e1dd9c9dbe
Ryan Borger <ryanborger1999@gmail.com>
2021-04-25 15:10:50 -0400

    SUpport page stuff

---
+++ src/CustomComponents/GenericButton/FormInputButton.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+class FormInputButton extends React.Component {
+  render() {
+    let color = this.props.color;
+    const buttonStyle = {
+      backgroundColor: color,
+      color: "black",
+      boxShadow: "0 8px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19)",
+      border: "none",
+      borderRadius: "80px",
+      fontSize: "11px",
+      outline: "none",
+      marginLeft: this.props.marginLeft,
+      maxWidth: this.props.maxWidth,
+      width: "auto",
+      height: this.props.height
+    };
+    
+    function changeBackground(e) {
+      e.target.style.background = 'white';
+    }
+
+    function originalBackground(e) {
+      e.target.style.background = color;
+    }
+    
+    return (
+      <input onMouseOver={changeBackground} onMouseLeave={originalBackground} style={buttonStyle} value="SEND" type="submit">
+        {this.props.description}
+      </input>
+    );
+  }
+}
+export default FormInputButton;
--- src/CustomComponents/Pages/BaraSupport.js
+++ src/CustomComponents/Pages/BaraSupport.js
@@ -1,10 +1,24 @@
 import React, { Component } from 'react';
+import SupportForm from "../SupportForm";
+import { Row, Container, Col } from 'reactstrap';
+import BaraFooter from '../BaraFooter';
 
-class BaraSupport extends Component{
+class BaraSupport extends Component {
+
     render(){
         return(
             <div className="backgroundPage">
-                <h1>Hello Support</h1>
+                <Container>
+                    <div>
+                        <Container>
+                            <h1 style={{textAlign: "center"}}>Customer Support</h1>
+                        </Container>
+                        <Container>
+                            <p style={{color: "white", textAlign: "center"}}>Email us with questions or concerns</p>
+                        </Container>
+                        <SupportForm></SupportForm>
+                    </div>
+                </Container>
             </div>
         );
     }
